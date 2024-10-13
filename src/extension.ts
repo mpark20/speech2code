8b766e14e91101bcef19199ca24ccb156d5e439e
Molly Park <mollysypark@gmail.com>
2024-10-12 21:18:27 -0700

    transcribes audio files to text editor

--- src/extension.ts
+++ src/extension.ts
@@ -2,48 +2,54 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from 'path';
-const WebSocket = require('ws');
-import express from 'express';
-import open from 'open';
 
-const app = express();
-const port = 9000;
-const websocketPort = 9001;
+import { AssemblyAI, RealtimeTranscriber, RealtimeTranscript } from 'assemblyai'
 
-app.use('/', express.static(path.join(__dirname, 'client')));
-app.listen(port, () => {
-	vscode.window.showInformationMessage(`[Speech to Text] Server running at localhost:${port}`);
-	open(`http://localhost:${port}`);
-});
+// AssemblyAI configuration
+const client = new AssemblyAI({
+  apiKey: 'a8f8800503f64f2cb716dd36b090f909' 
+})
+const audioFile = 'https://assembly.ai/nbc.mp3'
 
+const params = {
+  audio: audioFile,
+  speaker_labels: true
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "speech2code" is now active!');
-
-	const wss = new WebSocket.Server({port: websocketPort});
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('speech2code.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	const disposable = vscode.commands.registerCommand('speech2code.helloWorld', async () => {
 		vscode.window.showInformationMessage('Hello World from speech2code!');
-		wss.on('connection', (socket: any) => {
-			console.log('[Speech to Text] New WebSocket connection');
-			vscode.window.showInformationMessage('[Speech to Text] New WebSocket connection');
-
-			socket.on('message', (phrase: string) => {
-				console.log(`[Speech to Text] New WebSocket message: ${phrase}`);
-			});
-		});
+		run();
 	});
-
 	context.subscriptions.push(disposable);
+}
+
+// Runs transcription
+const run = async () => {
+	const transcript = await client.transcripts.transcribe(params)
+
+	if (transcript.status === 'error') {
+		console.error(`Transcription failed: ${transcript.error}`)
+		process.exit(1)
+	}
+
+	console.log(transcript.text)
+
+	const editor = vscode.window.activeTextEditor;
+	if (editor && typeof transcript.text === "string") {
+		const position = editor.selection.active;  // Current cursor position
+		const text: string = transcript.text
+		editor.edit(editBuilder => {
+			editBuilder.insert(position, text);  // Insert text at cursor position
+		});
+	}
+
+	for (let utterance of transcript.utterances!) {
+		console.log(`Speaker ${utterance.speaker}: ${utterance.text}`)
+	}
 }
 
 // This method is called when your extension is deactivated
---
+++ src/types/whisper-node.d.ts
@@ -0,0 +1,4 @@
+declare module 'whisper-node' {
+    const value: any;
+    export default value;
+}
